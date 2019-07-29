db39b5900d5357ba502102d7a63fcebe573491fb
Felix Martinez <felixaviermartinez@gmail.com>
2019-07-29 19:22:55 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,13 @@
-def take_a_name(name)
+def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+def introduction_with_language_with_optional(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  if language == language
+    language = ruby
+end
